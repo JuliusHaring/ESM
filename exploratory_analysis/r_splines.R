001ce0da788123b809e6aa9217381c073c9de496
Julius Haring <juliusharing@Juliuss-MacBook-Pro.local>
2020-01-03 12:08:11 +0100

    first working R version of hierarchical splines

---
+++ exploratory_analysis/r_splines.R
@@ -0,0 +1,43 @@
+library(splines)
+library(brms)
+library(ggplot2)
+setwd(getSrcDirectory()[1])
+
+test_mode <- FALSE
+
+filtered <- read.csv("../data/filtered.csv")
+
+patient_grp <- filtered[filtered$REGISTRATION_ID < 2,]
+test_grp <- filtered[filtered$REGISTRATION_ID > 2,]
+
+columns <- names(patient_grp)
+columns <- columns[grepl("ESM_", columns)]
+columns <- columns[-1] # Removes ESM_ABDPAIN (target)
+
+if(test_mode){
+  patient_grp <- patient_grp[0:100,]
+  columns <- c("ESM_GAS2","ESM_GAS1")
+}
+
+for(marker in columns){
+  try({
+    formula <- paste("ESM_ABDPAIN~","s(",marker,",by=REGISTRATION_ID, bs='fs')")
+    if(test_mode){
+      formula <- paste("ESM_ABDPAIN~","s(",marker,")")
+    }
+    formula <- as.formula(formula)
+    print(formula)
+    print(paste("NA: ", sum(is.na(patient_grp[marker]))))
+    print(paste("Availible:", sum(!is.na(patient_grp[marker]))))
+    print()
+  
+    fit <- brm(formula, data=patient_grp, iter=2000, chains=1)
+    summary(fit)
+    
+    fname <- paste("../images/",marker,".jpeg", sep="")
+    jpeg(filename = fname,width=3.25,height=3.25,units="in",res=300)
+    print(plot(marginal_effects(fit), ask=FALSE, plot=FALSE)[[1]] + ggplot2::ylim(0, 10) + ggplot2::xlim(0,10) + ggplot2::ggtitle(paste("ESM_ABDPAIN vs.",marker)))
+    dev.off()
+    dev.off()
+  })
+}
